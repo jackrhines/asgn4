39e1f05e34c1e6749a9005a0583db29883a4d9dc
Jack <jrhines@unix1.csc.calpoly.edu>
2022-05-09 18:17:21 -0700

    added README & cleaned up code a tiny bit more

--- directory.c
+++ directory.c
@@ -9,38 +9,6 @@
 #include "list.h"
 #include "directory.h"
 
-void open_directory(char *name, int layers, int hidden, int access) {
-	DIR *dirp;
-	struct dirent *cur = NULL;
-	char *temp;
-	name_list *root = NULL;		
-
-	dirp = opendir(name);
-	if (dirp == NULL) {
-		perror("Directory DNE");
-		return;
-	}
-	chdir(name);
-
-	printf("opened directory!\n");
-	/* read contents of directory into a sorted linked list */	
-	while ((cur = readdir(dirp)) != NULL) {
-		/* printf("directory item: %s\n", cur->d_name);*/
-		if ((strcmp(cur->d_name, ".") != 0) && (strcmp(cur->d_name, "..") != 0)) {
-			temp = strdup(cur->d_name);	
-			/*printf("name: %s\n", temp);*/
-			add_node(&root, temp, cur->d_type);
-		}
-	}
-
-	print_list(root);
-
-	/* print directory contents in tree-like structure */
-	print_tree(root, layers, hidden, access);
-	/* close the directory! */
-	return;
-}
-
 void print_dir (char *dir_name, char *layer, int hidden, int acc) {
     int n; /* Used to keep track of number of subdirectories and files */
     int i; /* Used to iterate through namelist */
@@ -104,7 +72,6 @@ void print_dir (char *dir_name, char *layer, int hidden, int acc) {
             /* Skip . & .. and any hidden files if hidden flag is low */ 
             if ( strcmp(sub_name, ".") && strcmp(sub_name, "..")) {
                 if (sub_name[0] != '.' || hidden) {
-                    /* printf("Sub: %s\n", (namelist[i])->d_name); */
                     print_dir( (namelist[i])->d_name, tmp, hidden, acc);
                 }
             }
